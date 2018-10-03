fac1c3e44a9cb87a4b555296189b943cfdd938b3
drake-jin <dydwls121200@gmail.com>
2018-10-03 23:35:19 +0900

    mobx를 이용한 store를 쓴 Counter예제

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'mobx-react';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import CounterStore from './stores/counter'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const counter = new CounterStore(); // 스토어 인스턴스 생성
+
+
+ReactDOM.render(
+  (
+    <Provider counter={counter}>
+      <App />
+    </Provider>
+  ),
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
